ef1ed2a6965a8b0eab827cffbaf5f3e07bc7ba52
John Meguerian <johnmeguerian@gmail.com>
2019-04-29 14:00:36 -0400

    hook up alert modal

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
 const roarButton = document.querySelector("#roar");
+const alert = document.querySelector('.Alert')
 
-roarButton.addEventListener('click', () => {alert("RAWR")})
+roarButton.addEventListener('click', function(event) {
+  roar()})
+
+function roar() {
+  alert.classList.add('Alert--active')
+  setTimeout(() => {
+    alert.classList.remove('Alert--active')
+  }, 5000);
+}
