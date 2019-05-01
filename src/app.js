c72652acb9b7bff0b286c61ab0a3a2099c971714
John Meguerian <johnmeguerian@gmail.com>
2019-05-01 11:18:21 -0400

    wire up tts modal

--- src/app.js
+++ src/app.js
@@ -1,12 +1,46 @@
-const roarButton = document.querySelector("#roar");
-const alert = document.querySelector('.Alert')
+const roarButton = document.querySelector('#js-roar-button')
+const ttsButton = document.querySelector('#js-tts-button')
+const roarModal = document.querySelector('#js-roar-modal')
+const ttsModal = document.querySelector('#js-tts-modal')
+const modalCancel = document.querySelector('#js-modal-cancel')
+
+const ttsForm = document.querySelector('#js-tts-form')
+
+modalCancel.addEventListener('click', function(event) {
+  let el = event.target
+  let theModal = el.closest('.Modal--active')
+  theModal.classList.remove('Modal--active')
+})
 
 roarButton.addEventListener('click', function(event) {
-  roar()})
+  console.log(`Fluffy Roars!`)
+  activateModalWithTimeout(roarModal)
+})
 
-function roar() {
-  alert.classList.add('Alert--active')
+ttsButton.addEventListener('click', function(event) {
+  activateModal(ttsModal)
+  document.querySelector('#js-tts-input').focus()
+})
+
+function activateModalWithTimeout(el) {
+  el.classList.add('Modal--active')
   setTimeout(() => {
-    alert.classList.remove('Alert--active')
-  }, 5000);
+    el.classList.remove('Modal--active')
+  }, 3000)
+}
+
+function activateModal(el) {
+  el.classList.add('Modal--active')
+}
+
+function removeModal(el) {
+  el.classList.remove('Modal--active')
+}
+
+ttsForm.onsubmit = function(event) {
+  let value = document.querySelector('#js-tts-input').value
+  event.preventDefault()
+  alert(`Fluffy Says: ${value}`)
+  console.log(`Fluffy Says: ${value}`)
+  removeModal(ttsModal)
 }
