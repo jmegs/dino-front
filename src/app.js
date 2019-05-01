7f2287f958027c25b63331afc872b54843830528
John Meguerian <johnmeguerian@gmail.com>
2019-05-01 12:21:14 -0400

    cool blinky buttons

--- src/app.js
+++ src/app.js
@@ -1,31 +1,13 @@
-const roarButton = document.querySelector('#js-roar-button')
 const ttsButton = document.querySelector('#js-tts-button')
-const roarModal = document.querySelector('#js-roar-modal')
+const ttsForm = document.querySelector('#js-tts-form')
 const ttsModal = document.querySelector('#js-tts-modal')
+
+const roarButton = document.querySelector('#js-roar-button')
+const roarModal = document.querySelector('#js-roar-modal')
+
 const modalCancel = document.querySelector('#js-modal-cancel')
 
-const ttsForm = document.querySelector('#js-tts-form')
-
-modalCancel.addEventListener('click', function(event) {
-  let el = event.target
-  let theModal = el.closest('.Modal--active')
-  theModal.classList.remove('Modal--active')
-})
-
-roarButton.addEventListener('click', function(event) {
-  // `GET /pre/<name>`
-  console.log(`Fluffy Roars!\nGET /pre/<name>`)
-  activateModalWithTimeout(roarModal)
-  fetch(`/pre/roar2`)
-    .then(res => console.log(`Responded ${res.status}`))
-    .catch(err => console.error(err))
-})
-
-ttsButton.addEventListener('click', function(event) {
-  activateModal(ttsModal)
-  document.querySelector('#js-tts-input').focus()
-})
-
+// Modal Utilities
 function activateModalWithTimeout(el) {
   el.classList.add('Modal--active')
   setTimeout(() => {
@@ -41,13 +23,60 @@ function removeModal(el) {
   el.classList.remove('Modal--active')
 }
 
+modalCancel.addEventListener('click', function(event) {
+  let el = event.target
+  let theModal = el.closest('.Modal--active')
+  theModal.classList.remove('Modal--active')
+})
+
+roarButton.addEventListener('click', function(event) {
+  activateModalWithTimeout(roarModal)
+  fetch(`/pre/roar2`)
+    .then(res => console.log(`Responded ${res.status}`))
+    .catch(err => console.error(err))
+})
+
+ttsButton.addEventListener('click', function(event) {
+  activateModal(ttsModal)
+  document.querySelector('#js-tts-input').focus()
+})
+
+// Submit TTS form to say endpoint
 ttsForm.onsubmit = function(event) {
-  // `GET /tts?say=<text>`
-  let value = document.querySelector('#js-tts-input').value
   event.preventDefault()
-  console.log(`Fluffy Says: ${value}`)
+  let value = document.querySelector('#js-tts-input').value
   fetch(`/tts?say=${encodeURIComponent(value)}`)
     .then(res => console.log(`Responded ${res.status}`))
     .catch(err => console.error(err))
   removeModal(ttsModal)
 }
+
+// Blinking Buttons
+const buttons = document.querySelectorAll('.Buttons__Panel .Button')
+
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * max)
+}
+
+function clearButtons() {
+  buttons.forEach(button => {
+    button.classList.remove('Button--Active')
+  })
+}
+
+function lightRandomButtons() {
+  const total = buttons.length
+  let newIndexes = [
+    getRandomNumber(total),
+    getRandomNumber(total),
+    getRandomNumber(total)
+  ]
+  for (let index of newIndexes) {
+    buttons[index].classList.add('Button--Active')
+  }
+}
+
+setInterval(() => {
+  clearButtons()
+  lightRandomButtons()
+}, 1000)
